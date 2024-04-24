c302bc7194120f4c82e6f69f42bc39f20e234a06
Priyanshu Gupta <priyanshu16095@gmail.com>
2024-04-24 19:48:07 +0530

    updated padding of home components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,17 +8,7 @@ function Contact() {
         <div className="contactContainer flex-v lightpContainer">
           <div className="contactHeader flex-h">
             <p className="title">Contact</p>
-            <p>React Hook Form</p>
-          </div>
-          <div className="contactInputs flex-v">
-            <div className="contactInputsUpper flex">
-              <input type="text" placeholder='Name' className='contactInput contactName' />
-              <input type="text" placeholder='Email' className='contactInput contactEmail' />
-            </div>
-            <input type="text" placeholder='Message' className='contactInput contactMessage' />
-          </div>
-          <div className="contactButton flex-e">
-            <button className="button">Send</button>
+            <p className='label'>React Hook Form</p>
           </div>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,19 +8,21 @@ import MenuIcon from '@mui/icons-material/Menu';
 function Navbar() {
   return (
     <div className='navbar'>
-      <div className="navbarContainer lightpContainer flex-s">
-        <div className="navbarLeft flex">
-          <div className="round"><img src={mine_img} className='mine_img' /></div>
-          <div className="info flex-v">
-            <p className="name">Priyanshu Gupta</p>
-            <p className="email">priyanshu@gmail.com</p>
+      <div className="navbarBody">
+        <div className="navbarContainer lightpContainer flex-s">
+          <div className="navbarLeft flex">
+            <div className="round"><img src={mine_img} className='mine_img' /></div>
+            <div className="info flex-v">
+              <p className="name">Priyanshu Gupta</p>
+              <p className="email">priyanshu@gmail.com</p>
+            </div>
           </div>
-        </div>
-        <div className="navbarRight flex">
-          <button className="button">Download CV</button>
-          <div className="round"><LinkedInIcon /></div>
-          <div className="round"><GitHubIcon /></div>
-          <div className="round"><MenuIcon /></div>
+          <div className="navbarRight flex">
+            <button className="button">Download CV</button>
+            <div className="round"><LinkedInIcon /></div>
+            <div className="round"><GitHubIcon /></div>
+            <div className="round"><MenuIcon /></div>
+          </div>
         </div>
       </div>
     </div>
