7dfd16d2b18cabd12cf3092f0ba682f0739c2ac5
Priyanshu Gupta <priyanshu16095@gmail.com>
2024-04-26 19:24:04 +0530

    some changes in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,7 @@ function Footer() {
     <div className='footer'>
         <div className="footerbody">
             <div className="footerContainer flex">
-                <p className="key">Developed by Priyanshu Gupta |</p>
-                <p className="key">April 24, 2024</p>
+                <p className="key">Developed by Priyanshu Gupta | April 24, 2024</p>
             </div>
         </div>
     </div>
