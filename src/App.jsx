6d7d19b9bdfb6d7b1fa7850233319c3b559ca197
Priyanshu Gupta <priyanshu16095@gmail.com>
2024-04-28 22:46:36 +0530

    ui made consistent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Home from './components/Home'
 import { ThemeProvider } from './theme-context'
+import LocomotiveScroll from 'locomotive-scroll';
+
+// const locomotiveScroll = new LocomotiveScroll()
 
 function App() {
   return (
