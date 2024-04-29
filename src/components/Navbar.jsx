1efd1b170d32b482556142206c88d722fb45244b
Priyanshu Gupta <priyanshu16095@gmail.com>
2024-04-30 00:20:46 +0530

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,59 +8,59 @@ import DarkModeIcon from '@mui/icons-material/DarkMode';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useTheme } from '../theme-context';
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 function Navbar() {
-  const fullRef = useRef(null);
+  // const fullRef = useRef(null);
 
-  useEffect(() => {
-    const tl = gsap.timeline({ paused: true });
+  // useEffect(() => {
+  //   const tl = gsap.timeline({ paused: true });
 
-    tl.to(fullRef.current, {
-      right: 0,
-      duration: 0.8,
-    })
-      .from(fullRef.current.querySelectorAll('.navbarOpen p'), {
-        x: 150,
-        duration: 0.7,
-        stagger: 0.3,
-        opacity: 0,
-      })
-      .from(fullRef.current.querySelector('.navbarOpen .closeIcon'), {
-        opacity: 0,
-      });
+  //   tl.to(fullRef.current, {
+  //     right: 0,
+  //     duration: 0.8,
+  //   })
+  //     .from(fullRef.current.querySelectorAll('.navbarOpen p'), {
+  //       x: 150,
+  //       duration: 0.7,
+  //       stagger: 0.3,
+  //       opacity: 0,
+  //     })
+  //     .from(fullRef.current.querySelector('.navbarOpen .closeIcon'), {
+  //       opacity: 0,
+  //     });
 
-    tl.pause();
-    fullRef.current.animationTimeline = tl;
-  }, []);
+  //   tl.pause();
+  //   fullRef.current.animationTimeline = tl;
+  // }, []);
 
-  const handleOpen = () => {
-    fullRef.current.animationTimeline.play();
-  };
+  // const handleOpen = () => {
+  //   fullRef.current.animationTimeline.play();
+  // };
   
-  const handleClose = () => {
-    fullRef.current.animationTimeline.reverse();
-  };
+  // const handleClose = () => {
+  //   fullRef.current.animationTimeline.reverse();
+  // };
 
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className='navbar'>
       <div className="navbarBody">
-        <div className="navbarOpen flex-v" ref={fullRef}>
+        {/* <div className="navbarOpen flex-v" ref={fullRef}>
           <CloseIcon className='closeIcon' onClick={handleClose} />
           <p className="title">Home</p>
           <p className="title">Mini Projects</p>
           <p className="title">Contact</p>
           <p className="title">About</p>
-        </div>
+        </div> */}
 
         <div className="navbarContainer flex-e">
           <div className="navIcons flex">
             <div className="round" onClick={toggleTheme}>
               {theme === 'dark' ? <DarkModeIcon className='icon' /> : <LightModeIcon className='icon' />}
             </div>
-            <div className="round"><MenuIcon className='openIcon' onClick={handleOpen} /></div>
+            <div className="round"><MenuIcon className='openIcon icon' /></div>
           </div>
         </div>
       </div>
