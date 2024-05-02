ca89b7b51af14f3aac3d7724007e69e5f0699f0b
Priyanshu Gupta <priyanshu16095@gmail.com>
2024-05-03 03:25:33 +0530

    new ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,5 @@
 import React from 'react'
 import './style.css'
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import mine_img from '../assets/mine_img.jpg'
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io5";
-import { FaJava } from "react-icons/fa";
-import { FaReact } from "react-icons/fa";
-import { FaDocker } from "react-icons/fa";
-import { FaGitAlt } from "react-icons/fa";
-import { SiMysql } from "react-icons/si";
-import { SiSpringboot } from "react-icons/si";
-import Marquee from "react-fast-marquee";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
@@ -27,7 +15,7 @@ function Hero() {
             <h1 className='build'>I build things for the web</h1>
           </div>
           <p className='location'>Full Stack Development and Devops</p>
-          <p>I like to craft solid and scalable frontend products with great user experiences.</p>
+          <p className='like'>I like to craft solid and scalable frontend products with great user experiences.</p>
           <div className="resheroIcons flex">
             <a href="https://www.linkedin.com/in/priyanshu-gupta-960136303?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target='_blank' className='round'><LinkedInIcon className='icon' /></a>
             <a href="https://github.com/priyanshu16095" target='_blank' className='round'><GitHubIcon className='icon' /></a>
--- src/components/WhatIKnow.jsx
+++ src/components/WhatIKnow.jsx
@@ -10,6 +10,8 @@ import linuxSvgIcon from '../assets/linuxSvgIcon.svg'
 import gitSvgIcon from '../assets/gitSvgIcon.svg'
 import javaSvgIcon from '../assets/javaSvgIcon.svg'
 import pythonSvgIcon from '../assets/pythonSvgIcon.svg'
+import kubernetesvgIcon from '../assets/kubernetesSvgIcon.svg'
+import redisSvgIcon from '../assets/redisSvgIcon.svg'
 
 function WhatIKnow() {
     return (
@@ -19,35 +21,51 @@ function WhatIKnow() {
 
                     <p className="title">What I Know</p>
 
-                    <div className="skills flex-s">
-                        <div className="frontend flex-v">
-                            <p className="md-font">Frontend</p>
-                            <div className="icons flex">
-                                <img src={htmlIconSvg} className='skillsIcon' />
-                                <img src={cssSvgIcon} className='skillsIcon' />
-                                <img src={javascriptSvgIcon} className='skillsIcon' />
-                                <img src={reactSvgIcon} className='skillsIcon' />
-                            </div>
-                        </div>
-                        <div className="backend flex-v">
-                            <p className="md-font">Backend</p>
+                    <div className="skills flex-v">
+                        {/* <div className="languages flex-v">
+                            <p className="md-font">Languages and Tools</p>
                             <div className="icons flex">
                                 <img src={javaSvgIcon} className='skillsIcon' />
                                 <img src={pythonSvgIcon} className='skillsIcon' />
-                                <img src={springSvgIcon} className='skillsIcon' />
                             </div>
-                        </div>
-                        <div className="devops flex-v">
-                            <p className="md-font">Devops</p>
-                            <div className="icons flex">
-                                <img src={gitSvgIcon} className='skillsIcon' />
-                                <img src={dockerSvgIcon} className='skillsIcon' />
-                                <img src={linuxSvgIcon} className='skillsIcon' />
+                        </div> */}
+                        <div className="flex-s">
+                            <div className="frontend flex-v">
+                                <p className="md-font">Frontend</p>
+                                <div className="icons flex">
+                                    <img src={htmlIconSvg} className='skillsIcon' />
+                                    <img src={cssSvgIcon} className='skillsIcon' />
+                                    <img src={javascriptSvgIcon} className='skillsIcon' />
+                                    <img src={reactSvgIcon} className='skillsIcon' />
+                                </div>
+                            </div>
+                            <div className="backend flex-v">
+                                <p className="md-font">Backend</p>
+                                <div className="icons flex">
+                                    <img src={javaSvgIcon} className='skillsIcon' />
+                                    <img src={pythonSvgIcon} className='skillsIcon' />
+                                    <img src={springSvgIcon} className='skillsIcon' />
+                                </div>
+                            </div>
+                            <div className="devops flex-v">
+                                <p className="md-font">Devops</p>
+                                <div className="icons flex">
+                                    <img src={gitSvgIcon} className='skillsIcon' />
+                                    <img src={dockerSvgIcon} className='skillsIcon' />
+                                    <img src={linuxSvgIcon} className='skillsIcon' />
+                                </div>
+                            </div>
+                            <div className="learning flex-v">
+                                <p className="md-font">Learning</p>
+                                <div className="icons flex">
+                                    <img src={kubernetesvgIcon} className='skillsIcon' />
+                                    <img src={redisSvgIcon} className='skillsIcon' />
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    
+
                 </div>
             </div>
         </div>
