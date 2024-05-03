e8e9d550efc155712ea8578bcf4eddd76a3ac26d
Priyanshu Gupta <priyanshu16095@gmail.com>
2024-05-04 02:21:45 +0530

    new ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,14 +8,14 @@ function Hero() {
   return (
     <div className='hero'>
       <div className="heroBody">
-        <div className="responsiveHero flex-v">
+        <div className="heroContainer flex-v">
           <p className='md-font'>Hi, I'm</p>
           <div className="about">
             <h1 className='hi'>Priyanshu</h1>
             <h1 className='build'>I build things for the web</h1>
           </div>
           <p className='location'>Full Stack Development and Devops</p>
-          <p className='like'>I like to craft solid and scalable frontend products with great user experiences.</p>
+          {/* <p className='like'>I like to craft solid and scalable frontend products with great user experiences.</p> */}
           <div className="resheroIcons flex">
             <a href="https://www.linkedin.com/in/priyanshu-gupta-960136303?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target='_blank' className='round'><LinkedInIcon className='icon' /></a>
             <a href="https://github.com/priyanshu16095" target='_blank' className='round'><GitHubIcon className='icon' /></a>
