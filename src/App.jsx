cb1b3e40919fa1a461f7c04474e8eb79b3d5006c
Priyanshu Gupta <priyanshu16095@gmail.com>
2024-05-31 11:35:48 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Home from './components/Home'
 import { ThemeProvider } from './theme-context'
 import LocomotiveScroll from 'locomotive-scroll';
-
-// const locomotiveScroll = new LocomotiveScroll()
+import Confetti from 'react-confetti'
+import useWindowSize from 'react-use/lib/useWindowSize'
 
 function App() {
+  const { width, height } = useWindowSize()
+  const [showConfetti, setShowConfetti] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowConfetti(false)
+    }, 3000)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div className='app'>
       <div className="appBody">
+        <div className={`confetti-wrapper ${showConfetti ? 'show' : 'hide'}`}>
+          <Confetti width={width} height={height} />
+        </div>
         <ThemeProvider>
           <Home />
         </ThemeProvider>
